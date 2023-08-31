3525269c8f51717529f143cfd13ad4df785ed1ac
Divyasinghsb <divya.singh.cse.2020@miet.ac.in>
2023-08-31 19:29:36 +0530

    openapi docs for gh-pages

--- frontend/profile/index.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    let email = window.localStorage.getItem("health-connect-plus-email");
-    let type = window.localStorage.getItem("health-connect-plus-type");
-    document.getElementById("user-email").innerText = email;
-    document.getElementById("user-type").innerText = type;
-});
